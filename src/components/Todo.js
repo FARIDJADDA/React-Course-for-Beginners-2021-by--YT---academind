84de9ca41814ff507ba80cda6b754bf8318fa1eb
Djadda Farid <FARIDJADDA750@GMAIL.COM>
2021-10-27 11:00:44 +0200

    add Backdrop component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -17,14 +17,13 @@ function Todo(props) {
   return (
     <div className="card">
       <h2>{props.text}</h2>
-      <div className="acion">
+      <div className="acions">
         <button className="btn" onClick={handleClickDelete}>
           {props.textBtn}
         </button>
       </div>
       {modalIsOpen && <Modal />}
       {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
-      <Backdrop />
     </div>
   );
 }
