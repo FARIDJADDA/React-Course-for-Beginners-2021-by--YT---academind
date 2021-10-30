bed96722073c9bb1c85c502628f7b9c47e8cbfa1
FARIDJADDA <faridjadda750@gmail.com>
2021-10-30 04:10:31 +0200

    new app reformat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import Todo from "./components/Todo";
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
 
 function App() {
   return (
     <div>
-      <h1>My Todo</h1>
-      <Todo text="Title" textBtn="Delete" />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
