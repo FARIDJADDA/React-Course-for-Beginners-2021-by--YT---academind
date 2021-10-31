6c75a57d397803f0b37a49101b59b9d294ba11bf
FARIDJADDA <faridjadda750@gmail.com>
2021-10-31 20:46:36 +0100

    Add new MeetupItem & MeetupList componant on meetup folder

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,5 @@
+import MeetupList from "../components/layout/meetups/MeetupList";
+
 const DUMMY_DATA = [
   {
     id: "m1",
@@ -15,7 +17,7 @@ const DUMMY_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
     address: "Meetupstreet 5, 12345 Meetup City",
     description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+      "is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
   },
 ];
 
@@ -23,11 +25,7 @@ function AllMeetupsPage() {
   return (
     <section>
       <h1>All Meetups</h1>
-      <ul>
-        {DUMMY_DATA.map((meetup) => {
-          return <li key={meetup.id}>{meetup.title}</li>;
-        })}
-      </ul>
+      <MeetupList meetups={DUMMY_DATA} />
     </section>
   );
 }
